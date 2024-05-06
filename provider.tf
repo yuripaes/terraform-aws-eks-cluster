8a4db8e1e27807c9472922e919ffcf070246235f
Yuri Paes <yuri.paes@gmail.com>
2024-05-06 19:11:02 -0300

    feat: remote backend

--- provider.tf
+++ provider.tf
@@ -1,9 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.48.0"
     }
+  }
+  backend "s3" {
+    bucket = "terraform-infra-20240506"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
