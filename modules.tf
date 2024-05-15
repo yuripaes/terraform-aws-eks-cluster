670e95fbc2c23133fa95f4030a15bbd82b2b09a6
Yuri Paes <yuri.paes@gmail.com>
2024-05-15 16:27:40 -0300

    feat: initialized module and added module call

--- modules.tf
+++ modules.tf
@@ -12,3 +12,18 @@ module "eks_cluster" {
   public-subnet-1a = module.eks_network.subnet_pub_1a
   public-subnet-1b = module.eks_network.subnet_pub_1b
 }
+
+module "managed-node-group" {
+  source            = "./modules/managed-node-group"
+  cluster_name      = module.eks_cluster.cluster_name
+  project_name      = var.project_name
+  tags              = local.tags
+  private-subnet-1a = module.eks_network.subnet_priv_1a
+  private-subnet-1b = module.eks_network.subnet_priv_1b
+}
+
+module "aws-load-balancer-controller" {
+  source       = "./modules/aws-load-balancer-controller"
+  project_name = var.project_name
+  tags         = local.tags
+}
