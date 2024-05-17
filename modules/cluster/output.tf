53ef1aa23cc4b56260917ab7b6a2e1cde62abce8
Yuri Paes <yuri.paes@gmail.com>
2024-05-16 23:02:09 -0300

    fix: certificate_autority data

--- modules/cluster/output.tf
+++ modules/cluster/output.tf
@@ -11,5 +11,5 @@ output "endpoint" {
 }
 
 output "certificate_authority" {
-  value = aws_eks_cluster.eks_cluster.certificate_authority
+  value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
 }
