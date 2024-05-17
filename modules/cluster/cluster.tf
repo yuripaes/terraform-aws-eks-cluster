4180a9a0290c49fa36f788bd717d8c9ceded119f
Yuri Paes <yuri.paes@gmail.com>
2024-05-16 21:49:56 -0300

    style removed new line

--- modules/cluster/cluster.tf
+++ modules/cluster/cluster.tf
@@ -24,11 +24,3 @@ resource "aws_eks_cluster" "eks_cluster" {
     }
   )
 }
-
-output "endpoint" {
-  value = aws_eks_cluster.eks_cluster.endpoint
-}
-
-output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
-}
