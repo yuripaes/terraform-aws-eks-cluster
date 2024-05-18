fb3c20bb7746b8097761b29906dbaec1a6c674ea
Yuri Paes <yuri.paes@gmail.com>
2024-05-18 00:36:21 -0300

    style: formatting resource name

--- modules/managed-node-group/mng.tf
+++ modules/managed-node-group/mng.tf
@@ -1,4 +1,4 @@
-resource "aws_eks_node_group" "eks-managed-node-group" {
+resource "aws_eks_node_group" "eks_managed_node_group" {
   cluster_name    = var.cluster_name
   node_group_name = "${var.project_name}-nodegroup"
   node_role_arn   = aws_iam_role.eks_mng_role.arn
